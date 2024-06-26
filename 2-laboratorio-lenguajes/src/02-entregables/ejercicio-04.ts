2b04af21ddde7a8ff9009f69a6fce542d5d709fb
Gabriel Pretel <gabrilinex@gmail.com>
2024-06-26 16:12:05 +0200

    Finished fourth exercise

--- 2-laboratorio-lenguajes/src/02-entregables/ejercicio-04.ts
+++ 2-laboratorio-lenguajes/src/02-entregables/ejercicio-04.ts
@@ -1,1 +1,20 @@
 console.log("************** DELIVERABLE 04 *********************");
+interface Book {
+  title: string;
+  isRead: boolean;
+}
+
+const books = [
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true },
+];
+
+const isBookRead = (books: Book[], title: string): boolean => {
+  const book = books.find((book) => book.title === title);
+  return book ? book.isRead : false;
+};
+
+console.log(isBookRead(books, "Devastación")); // true
+console.log(isBookRead(books, "Canción de hielo y fuego")); // false
+console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
