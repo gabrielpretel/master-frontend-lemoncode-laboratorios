dcc1a30e0b46469410b9f7dc7f51fa90d24dcc2f
Gabriel Pretel <gabrilinex@gmail.com>
2024-06-26 14:09:55 +0200

    Finished second exercise

--- 2-laboratorio-lenguajes/src/02-entregables/ejercicio-02.ts
+++ 2-laboratorio-lenguajes/src/02-entregables/ejercicio-02.ts
@@ -1,2 +1,21 @@
 console.log("************** DELIVERABLE 02 *********************");
 
+const concat = (a: any[], b: any[]): any[] => {
+  const concatArray = [...a, ...b];
+  return concatArray;
+};
+
+console.log(concat([1, 2], [3, 4]));
+console.log(concat([1, 2], ["a", "b"]));
+console.log(concat(["a", "b", 0], ["c", 1, { name: "Gabriel" }]));
+
+// Nota: Estoy usando any[] por flexibilidad y que al menos asegure que lo que
+// recibe y devuelve la función sean arrays
+
+console.log("************** OPTIONAL *********************");
+
+const dynamicConcat = (...args: any[]): any[] => {
+  return args.reduce((acc, array) => [...acc, ...array], []);
+};
+
+console.log(dynamicConcat([1, 2], [3, 4], ["hello", "goodbye"], []));
