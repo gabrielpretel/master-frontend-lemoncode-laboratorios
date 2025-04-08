2fad8c6d40ef132ef0dbc7af12417f97b4b1b02f
Gabriel Pretel <gabrilinex@gmail.com>
2025-04-08 11:34:27 +0200

    feat: delete 7 lab

--- 7-laboratorio-cloud/basico/ejercicio-1/composables/useRandomColors.ts
+++
@@ -1,8 +0,0 @@
-const colors = ['#FFF8F5', '#FFF3ED', '#FFEDE4', '#FFE8DC', '#FFE2D3']
-
-export const useRandomColors = () => {
-  const getRandomColor = (): string =>
-    colors[Math.floor(Math.random() * colors.length)]
-
-  return { getRandomColor }
-}
--- 7-laboratorio-cloud/basico/ejercicio-1/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts">
-  import TheTaskListsView from '../components/TheTaskLists/TheTaskListsView.vue'
-</script>
-
-<template><TheTaskListsView /></template>
